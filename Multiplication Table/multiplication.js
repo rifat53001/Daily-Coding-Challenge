4daaf6ca5c6eaf46d1bf49c8078a5c525fc8e0e2
rifat53001 <khanr1@bxscience.edu>
2017-08-08 21:01:39 -0400

    multiplication table

---
+++ Multiplication Table/multiplication.js
@@ -0,0 +1,22 @@
+var color_td;
+document.write("<table border='1px'>");
+
+for(var i = 1; i < 13; i++) {
+
+	document.write("<tr style='height:30px;'>");
+
+	for(var j = 1; j < 13; j++) {
+
+		if(j == 1 || i == 1) {
+			color_td = "#ccc";
+		}
+		else {
+			color_td = "#fff";
+		}
+
+		document.write("<td style='width:30px;background-color:" + color_td + "'>" + i*j + "</td>");
+	}
+	document.write("</tr>");
+}
+
+document.write("</table>");
